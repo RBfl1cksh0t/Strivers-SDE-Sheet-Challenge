3b9a664ae1322fca4433018e181b7f4d404465e8
RBfl1cksh0t <rajarshibasu19198@gmail.com>
2023-06-05 05:36:09 -0600

    Commiting 2

--- src/Day1Arrays/SetMatrixZeros.java
+++ src/Day1Arrays/SetMatrixZeros.java
@@ -3,5 +3,28 @@ package Day1Arrays;
 public class SetMatrixZeros {
     public static void main(String[] args) {
 
+
+    }
+
+    static int[][] bruteset (int[][] twoDarr){
+
+        for (int i = 0; i < twoDarr.length; i++) {
+            for (int j = 0; j < twoDarr[0].length; j++) {
+                if (twoDarr[i][j] == 0){
+                    twoDarr[i][j] = -1;
+                }
+            }
+        }
+
+        for (int i = 0; i < twoDarr.length; i++) {
+            for (int j = 0; j < twoDarr[0].length; j++) {
+                if (twoDarr[i][j] == -1){
+                    twoDarr[i][j] = 0;
+                }
+            }
+
+        }
+
+        return twoDarr;
     }
 }
